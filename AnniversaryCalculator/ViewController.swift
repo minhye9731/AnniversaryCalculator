f680c0a3bc3c50b8786217d89815ae72687f6ec5
minhye9731 <jenney62639731@gmail.com>
2022-07-14 00:32:59 +0900

    홈 화면 UI 코드설정 및 datepicker 버전설정

--- AnniversaryCalculator/ViewController.swift
+++ AnniversaryCalculator/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var anniversaryBoxes: [UIImageView]!
+    @IBOutlet var darkEffectView: [UIView]!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUI()
     }
-
+    
+    func setUI() {
+        
+        // 기념일 박스 4개 UI 설정
+        for i in 0...(anniversaryBoxes.count - 1) {
+            anniversaryBoxes[i].layer.cornerRadius = 8
+            anniversaryBoxes[i].clipsToBounds = true
+            darkEffectView[i].layer.cornerRadius = 8
+            anniversaryBoxes[i].clipsToBounds = true
+            anniversaryBoxes[i].layer.shadowColor = UIColor.gray.cgColor
+            anniversaryBoxes[i].layer.shadowOpacity = 0.8
+            anniversaryBoxes[i].layer.shadowOffset = CGSize.zero
+            anniversaryBoxes[i].layer.shadowRadius = 4
+        }
+        
+        // datepicker - ios14이후에서도 wheel 스타일 적용되도록 설정
+        if #available(iOS 14.0, *) {
+            datePicker.preferredDatePickerStyle = .wheels
+        }
+    }
 
 }
 
